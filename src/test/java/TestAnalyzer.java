5fc8f7cf39da0f43f56f35e3a2c649fb09b48974
florentdupont <florent.dupont@gmail.com>
2013-07-23 17:25:48 +0200

    Renommage de la classe de test pour prise en compte par Maven

---
+++ src/test/java/TestAnalyzer.java
@@ -0,0 +1,46 @@
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.internal.runners.JUnit4ClassRunner;
+import org.junit.runner.RunWith;
+
+import com.dolphin.processor.DolphinAnalyzer;
+
+@RunWith(JUnit4ClassRunner.class)
+public class TestAnalyzer {
+
+		
+	@Test
+	public void test2() throws MalformedURLException {
+		// recupère la ressource dans les tests
+		//URL url = this.getClass().getClassLoader().getResource("test.jar-tests.jar");
+		
+		
+		
+		String userdir = System.getProperty("user.dir");
+		System.out.println(userdir);
+		String path = userdir + File.separator + "target" + File.separator + "test.jar-tests.jar";
+		
+		List<URL> jarPaths = new ArrayList<URL>();
+		
+		File f = new File(path);
+		
+		jarPaths.add(f.toURI().toURL());
+		
+		String export = userdir + File.separator + "target" + File.separator + "export";
+			
+		
+		DolphinAnalyzer analyzer = new DolphinAnalyzer(jarPaths, "com.test", export, "html", true);
+		analyzer.analyseClasses();
+		
+	}
+	
+	
+	
+	
+}
